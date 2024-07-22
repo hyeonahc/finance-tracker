959964e52e629f050b5153f02a7bedd81ee79016
Hyeonah Cho <hyeonah@Hyeonahs-MacBook-Air.local>
2024-07-22 17:32:19 -0400

    Add userRouter

--- apps/server/server.ts
+++ apps/server/server.ts
@@ -2,6 +2,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import mongoose from 'mongoose'
+import userRouter from 'routers/userRouter'
 
 dotenv.config()
 
@@ -25,6 +26,7 @@ connectDB()
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/users', userRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
---
+++ apps/server/src/routers/userRouter.ts
@@ -0,0 +1,8 @@
+import * as userController from 'controllers/userController'
+import { Router } from 'express'
+
+const router: Router = Router()
+
+router.post('/signup', userController.signupUser)
+
+export default router
