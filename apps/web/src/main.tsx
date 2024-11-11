4968db94ac9a05fc5939c30518b9477594545fe2
Hyeonah Cho <hyeonah@Hyeonahs-MacBook-Air.local>
2024-11-11 16:11:25 -0500

    Remove React.StrictMode

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
@@ -6,9 +5,7 @@ import "./index.css";
 import Providers from "./providers/Providers.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Providers>
-      <App />
-    </Providers>
-  </React.StrictMode>,
+  <Providers>
+    <App />
+  </Providers>,
 );
