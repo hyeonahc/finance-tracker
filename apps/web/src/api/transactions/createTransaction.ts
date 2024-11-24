424332b5dad313cc2794960ecb4b0a0ebcc14e77
Hyeonah Cho <hyeonah@Hyeonahs-Air.ht.home>
2024-11-24 16:38:27 -0500

    Create createTransaction API function

---
+++ apps/web/src/api/transactions/createTransaction.ts
@@ -0,0 +1,34 @@
+import { INewTransaction, ISavedTransaction } from "src/types/transactions";
+
+export interface ITransactionResponse {
+  message: string;
+  transaction: ISavedTransaction;
+}
+
+const API_BASE_URL = import.meta.env.VITE_API_LOCAL_8080;
+
+export const createTransaction = async (
+  id: string,
+  newTransactionData: Partial<INewTransaction>,
+): Promise<ITransactionResponse> => {
+  const token = localStorage.getItem("token");
+
+  const response = await fetch(`${API_BASE_URL}/api/transactions/${id}`, {
+    body: JSON.stringify(newTransactionData),
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  });
+
+  console.log("createTransaction api response: ", response);
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message);
+  }
+
+  const responseData = await response.json();
+  return responseData;
+};
