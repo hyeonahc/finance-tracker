74acd92352dcd28fd35d7c0c475bcfa1e870a5d7
Hyeonah Cho <hyeonah.hello@gmail.com>
2024-11-24 19:14:50 -0500

    Remove a comment in useCreateTransaction

--- apps/web/src/hooks/transactions/useCreateTransaction.ts
+++ apps/web/src/hooks/transactions/useCreateTransaction.ts
@@ -2,7 +2,6 @@ import { createTransaction } from "@api/transactions/createTransaction";
 import { useMutation } from "@tanstack/react-query";
 import { INewTransaction } from "src/types/transactions";
 
-//
 export const useCreateTransaction = (options = {}) => {
   return useMutation({
     mutationFn: (newTransactionData: INewTransaction) =>
