1407756519de8e13a35594a12452477eb66c9c82
Hyeonah Cho <hyeonah@Hyeonahs-Air.ht.home>
2024-11-24 19:04:53 -0500

    Create useCreateTransaction hook

--- apps/web/src/hooks/transactions/useCreateTransaction.ts
+++ apps/web/src/hooks/transactions/useCreateTransaction.ts
@@ -2,6 +2,7 @@ import { createTransaction } from "@api/transactions/createTransaction";
 import { useMutation } from "@tanstack/react-query";
 import { INewTransaction } from "src/types/transactions";
 
+//
 export const useCreateTransaction = (options = {}) => {
   return useMutation({
     mutationFn: (newTransactionData: INewTransaction) =>
