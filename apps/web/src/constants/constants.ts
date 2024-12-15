01cda48a819ca2e758dc4c5dade4be53098e1bf5
Hyeonah Cho <hyeonah.hello@gmail.com>
2024-12-15 17:06:29 -0500

    Refactor constants to use named exports only and remove unnecessary default export

--- apps/web/src/constants/constants.ts
+++ apps/web/src/constants/constants.ts
@@ -1,9 +1,3 @@
 export const BOTTOM_NAV_HEIGHT = "80px";
 export const LAYOUT_PADDING = 5;
 export const BORDER_RADIUS = "4px";
-
-export default {
-  BORDER_RADIUS,
-  BOTTOM_NAV_HEIGHT,
-  LAYOUT_PADDING,
-};
