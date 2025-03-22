2c8b64ed0b67a584a2c7d12213a3c9cb4351bf14
Hyeonah Cho <hyeonah.hello@gmail.com>
2025-03-22 15:44:56 -0400

    Setup testing environment with Vitest and jsdom

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +9,9 @@ export default defineConfig({
     include: ["react", "react-dom"],
   },
   plugins: [react(), tsconfigPaths()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./setupTests.ts",
+  },
 });
