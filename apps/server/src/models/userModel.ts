393462ab1ce38061fbcb54baffc1a32dceec95fe
Hyeonah <hyeonah.hello@gmail.com>
2025-04-19 15:56:02 -0400

    Change library bcrypt to bcryptjs (#26)
    * Prepare backend for Render deployment
    
    * Update tsconfig.json in server for deployment
    
    * Resolve alias paths with tsc-alias
    
    * Add missing files to last commit message
    
    * Update baseUrl
    
    * Update baseUrl and Fix authMiddleware import
    
    * Change bcrypt library to bcryptjs

--- apps/server/src/models/userModel.ts
+++ apps/server/src/models/userModel.ts
@@ -1,5 +1,5 @@
 import { IUserModel } from "@interfaces/IUser";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { Schema, model } from "mongoose";
 import validator from "validator";
 
