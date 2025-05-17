8ccdda6aae8c57db6d2a7b331459ce77616dd492
Hyeonah Cho <hyeonah.hello@gmail.com>
2025-05-17 15:20:58 -0400

    Add YearMonthPicker component to AnalysisReport page

--- apps/web/src/pages/AnalysisReport.tsx
+++ apps/web/src/pages/AnalysisReport.tsx
@@ -1,7 +1,18 @@
-import MaintenanceMode from "@components/views/MaintenanceMode";
+import YearMonthPicker from "@components/filters/YearMonthPicker";
+import { Box } from "@mui/material";
+import { useDateFilterStore } from "src/store/useDateFilterStore";
 
 const AnalysisReport = () => {
-  return <MaintenanceMode pageName="Analysis & Report page" />;
+  const { dateDisplayMode, selectedDate } = useDateFilterStore();
+
+  return (
+    <Box>
+      <YearMonthPicker
+        dateDisplayMode={dateDisplayMode}
+        selectedDate={selectedDate}
+      />
+    </Box>
+  );
 };
 
 export default AnalysisReport;
